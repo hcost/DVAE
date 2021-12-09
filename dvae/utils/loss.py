e2eda6725d0be81c17f06ac7563968ade7cb523f
Harrison <harrisoncostantino@berkeley.edu>
2021-12-08 22:33:47 -0800

    sync

--- dvae/utils/loss.py
+++ dvae/utils/loss.py
@@ -10,28 +10,29 @@ License agreement in LICENSE.txt
 import torch
 
 def loss_ISD(x, y):
-    y = y + 1e-10
-    ret = torch.sum( x/y - torch.log(x/y) - 1)
-    return ret
+	y = y + 1e-10
+	ret = torch.sum( x/y - torch.log(x/y) - 1)
+	return ret
 
 def loss_KLD(z_mean, z_logvar, z_mean_p=0, z_logvar_p=0):
-    ret = -0.5 * torch.sum(z_logvar - z_logvar_p 
-                - torch.div(z_logvar.exp() + (z_mean - z_mean_p).pow(2)+1e-8, z_logvar_p.exp()+1e-8))
-    return ret
+	print(z_logvar.exp(), (z_mean - z_mean_p).pow(2), z_logvar_p.exp()+1e-8)
+	ret = -0.5 * torch.sum(z_logvar - z_logvar_p
+				- torch.div(z_logvar.exp() + (z_mean - z_mean_p).pow(2)+1e-8, z_logvar_p.exp()+1e-8))
+	return ret
 
 def loss_JointNorm(x, y, nfeats=3):
-    seq_len, bs, _ = x.shape
-    x = x.reshape(seq_len, bs, -1, nfeats)
-    y = y.reshape(seq_len, bs, -1, nfeats)
-    ret = torch.sum(torch.norm(x-y, dim=-1))
-    return ret
+	seq_len, bs, _ = x.shape
+	x = x.reshape(seq_len, bs, -1, nfeats)
+	y = y.reshape(seq_len, bs, -1, nfeats)
+	ret = torch.sum(torch.norm(x-y, dim=-1))
+	return ret
 
 def loss_MPJPE(x, y, nfeats=3):
-    seq_len, bs, _ = x.shape
-    x = x.reshape(seq_len, bs, -1, nfeats)
-    y = y.reshape(seq_len, bs, -1, nfeats)
-    ret = (x-y).norm(dim=-1).mean(dim=-1).sum()
-    return ret
+	seq_len, bs, _ = x.shape
+	x = x.reshape(seq_len, bs, -1, nfeats)
+	y = y.reshape(seq_len, bs, -1, nfeats)
+	ret = (x-y).norm(dim=-1).mean(dim=-1).sum()
+	return ret
 
 # def loss_ISD(x, y):
 #     seq_len, bs, _ = x.shape
