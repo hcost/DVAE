e54d730adbec8ab51a6fd1e7a55682e0ef97dc80
Harrison <harrisoncostantino@berkeley.edu>
2021-12-08 22:54:07 -0800

    sync

--- dvae/utils/loss.py
+++ dvae/utils/loss.py
@@ -15,7 +15,7 @@ def loss_ISD(x, y):
 	return ret
 
 def loss_KLD(z_mean, z_logvar, z_mean_p=0, z_logvar_p=0):
-	ret = -0.5 * torch.sum(z_logvar - z_logvar_p - torch.div(z_logvar.exp() + (z_mean - z_mean_p + 1e-5).pow(2), z_logvar_p.exp()+1e-5))
+	ret = -0.5 * torch.sum(z_logvar - z_logvar_p - torch.div(z_logvar.exp() + (z_mean - z_mean_p + 1e-5).pow(2), z_logvar_p.exp()+1e-3))
 	return ret
 
 def loss_JointNorm(x, y, nfeats=3):
